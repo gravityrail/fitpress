5c86b2100bd8fc9e597d7f4c41fcf79efdf51308
Daniel Walmsley <goldsounds@gmail.com>
2015-04-07 12:24:56 -0700

    Initial commit

---
+++ fitbit.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: FitBit
+Version: 0.1-alpha
+Description: Publish your FitBit statistics on your WordPress blog
+Author: Daniel Walmsley
+Author URI: http://danwalmsley.com
+Plugin URI: http://github.com/gravityrail/wp-fitbit-plugin
+Text Domain: fitbit
+Domain Path: /languages
+*/
+
